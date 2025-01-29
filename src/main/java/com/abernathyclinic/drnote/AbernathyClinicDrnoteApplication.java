e2873c28544cf147eb1e617226c5f6cb6c889dd6
github username <github email address>
2025-01-29 13:08:26 -0500

    first commit

---
+++ src/main/java/com/abernathyclinic/drnote/AbernathyClinicDrnoteApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.drnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbernathyClinicDrnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbernathyClinicDrnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abernathyclinic/drnote/AbernathyClinicDrnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.drnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbernathyClinicDrnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
