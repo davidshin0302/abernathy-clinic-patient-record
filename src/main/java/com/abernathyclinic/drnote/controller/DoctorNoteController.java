fd8366450856bd44f98da9b161bc6484860b6a62
github username <github email address>
2025-02-12 11:39:14 -0500

    Added CrosOrigin to allow front end to fetch data without blocking.

--- src/main/java/com/abernathyclinic/drnote/controller/DoctorNoteController.java
+++ src/main/java/com/abernathyclinic/drnote/controller/DoctorNoteController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +16,7 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequestMapping("/patHistory")
+@CrossOrigin(origins = "http://localhost:3000")
 public class DoctorNoteController {
     @Autowired
     private DoctorNoteRepository doctorNoteRepository;
